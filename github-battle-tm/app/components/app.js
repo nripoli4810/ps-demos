710b8c896aa9b907d02a314d13d58d97f602955a
Nate Ripoli <nripoli4810@users.noreply.github.com>
2019-05-16 15:42:00 -0400

    React Fundamentals: Routers

--- github-battle-tm/app/components/app.js
+++ github-battle-tm/app/components/app.js
@@ -1,12 +1,30 @@
 var React = require('react')
+var ReactRouter = require('react-router-dom')
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route
+var Nav = require('./nav')
+var Switch = ReactRouter.Switch
+
 var Popular = require('./Popular')
+var Home = require('./home')
+var Battle = require('./battle')
 
 class App extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <Router>
+                <div className='container'>
+                    <Nav />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/popular' component={Popular} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route render={function () {
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
